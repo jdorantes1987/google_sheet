280332fc9750cf209e58be47ec69df3d9c4bea64
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-03-11 16:50:48 -0400

    Se agregó información para pruebas de inserción de data

--- escribir.py
+++ escribir.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
@@ -12,13 +13,59 @@ service = build("sheets", "v4", credentials=creds)
 sheet = service.spreadsheets()
 
 # Debe ser una matriz por eso el doble [[]]
-values = [["V183291145", "JACKSON DORANTES", "MASCULINO"]]
+values = [
+    [
+        "V28154135",
+        "ANTHONY CÁCERES",
+        "MASCULINO",
+        None,
+        23,
+        "SOLTER@",
+        "04241519249",
+        "APOSENTO ALTO",
+        "MATRIMONIO JÓVEN",
+        "ESPOSOS DEFITT",
+        None,
+        "JACKSON DORANTES",
+        "ENCUENTRISTA",
+    ],
+    [
+        "V183291145",
+        "JACKSON DORANTES",
+        "MASCULINO",
+        datetime.strptime("19870806", "%Y%m%d").strftime("%m-%d-%Y"),
+        37,
+        "CASAD@",
+        "04143893828",
+        "APOSENTO ALTO",
+        "MATRIMONIO JÓVEN",
+        "DIOS",
+        None,
+        None,
+        "SERVIDOR",
+    ],
+    [
+        "V15152791",
+        "MIRTHA GRATEROL",
+        "FEMENINO",
+        datetime.strptime("19801122", "%Y%m%d").strftime("%m-%d-%Y"),
+        45,
+        "CASAD@",
+        "04146126353",
+        "APOSENTO ALTO",
+        "MATRIMONIO JÓVEN",
+        "DIOS",
+        None,
+        None,
+        "SERVIDOR",
+    ],
+]
 # Llamamos a la api
 result = (
     sheet.values()
     .append(
         spreadsheetId=SPREADSHEET_ID,
-        range="Tabla_1!A1:C1",
+        range="DATA",
         valueInputOption="USER_ENTERED",
         body={"values": values},
     )
